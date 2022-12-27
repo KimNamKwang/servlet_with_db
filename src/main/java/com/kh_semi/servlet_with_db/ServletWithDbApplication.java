ebf2d8e4c1a0cfd74eb28dd4461bf204eaf0bfff
KimNamKwang <durante0916@gmail.com>
2022-12-27 14:06:57 +0900

    first commit

---
+++ src/main/java/com/kh_semi/servlet_with_db/ServletWithDbApplication.java
@@ -0,0 +1,13 @@
+package com.kh_semi.servlet_with_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletWithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletWithDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh_semi/servlet_with_db/ServletWithDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh_semi.servlet_with_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletWithDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
