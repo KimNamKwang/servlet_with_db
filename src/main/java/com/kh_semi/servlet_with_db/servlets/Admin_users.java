336748d7e2ae20d22d4e06630d0e03986c3523ed
AnJiYeong <memarcis2@naver.com>
2022-12-29 15:19:51 +0900

    up

--- src/main/java/com/kh_semi/servlet_with_db/servlets/Admin_users.java
+++ src/main/java/com/kh_semi/servlet_with_db/servlets/Admin_users.java
@@ -1,6 +1,12 @@
 package com.kh_semi.servlet_with_db.servlets;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.kh_semi.servlet_with_db.dao.JiyeongDB;
+import com.kh_semi.servlet_with_db.dao.PollWithDB;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -11,11 +17,30 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/Admin/users")
 public class Admin_users extends HttpServlet{
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)throws IOException, ServletException {
+        String userId = request.getParameter("USER_ID");
+
+       JiyeongDB jiyeongDB = new JiyeongDB();
+        ArrayList<HashMap> user_list = null;
+        try {
+            user_list = jiyeongDB.getUsers(userId);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        for(int i = 0; i < user_list.size(); i++){
+            HashMap<String, Object> user = user_list.get(i);
+            System.out.println(user.get("NAME"));
+            System.out.println(user.get("USER_ID"));
+            System.out.println(user.get("PASSWORD"));
+            System.out.println(user.get("PHONE_NUMBER"));
+            System.out.println(user.get("USER_EMAIL"));
+            System.out.println(user.get("ADDRESS"));
+            System.out.println(user.get("ADDRESSADD"));
+        }
+
         response.setContentType("text/html;charset=UTF-8"); // 응답을 보낼 때 한글이 깨지지 않게 해주는 것. 백엔드를 위한 것
 
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("");
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/admin_users.jsp");
         requestDispatcher.forward(request, response);
 
     }
