5e4e71b42f478ddb360cf3f5bb6b6ab8168e62f8
tjrwls234 <tjrwls234@gmail.com>
2022-12-29 15:17:02 +0900

    add index

--- src/main/java/com/kh_semi/servlet_with_db/servlets/index_servlet.java
+++ src/main/java/com/kh_semi/servlet_with_db/servlets/index_servlet.java
@@ -13,7 +13,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@WebServlet(urlPatterns = "/main")
+@WebServlet(urlPatterns = "")
 public class index_servlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
