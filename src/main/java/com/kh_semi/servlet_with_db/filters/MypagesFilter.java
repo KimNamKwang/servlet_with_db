ae51d646f64683eea1417404f64a8bb76d269b1b
KimNamKwang <durante0916@gmail.com>
2022-12-30 14:23:54 +0900

    필터 일단 만들어만 놓기

---
+++ src/main/java/com/kh_semi/servlet_with_db/filters/MypagesFilter.java
@@ -0,0 +1,49 @@
+package com.kh_semi.servlet_with_db.filters;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebFilter("/myPage/*")
+public class MypagesFilter implements Filter {
+
+    @Override
+    public void destroy() {
+
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        System.out.println(request.getRemoteHost());
+        System.out.println(request.getRemoteAddr());
+
+        /* url을 빼내기 위해 HttpServletRequest으로 선언 */
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        String uri = httpServletRequest.getRequestURI();
+
+        if (uri.endsWith("Servlets")) {
+            /* index.html로 보내줬다가 */
+            httpServletResponse.sendRedirect("index.html");
+        } else {
+            /* chain이 url 서블릿으로 넘겨주는 역할. 곧 Dispatcher */
+            chain.doFilter(request, response);
+        }
+
+    }
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+}
--- src/main/java/com/kh_semi/servlet_with_db/servlets/Survey_submitServlet.java
+++ src/main/java/com/kh_semi/servlet_with_db/servlets/Survey_submitServlet.java
@@ -13,7 +13,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@WebServlet(urlPatterns = "/Survey_submitServlet")
+@WebServlet(urlPatterns = "/survey/SubmitServlet")
 public class Survey_submitServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
