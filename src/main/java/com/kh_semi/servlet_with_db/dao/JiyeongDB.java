0d8fc52e8e1a55ced07dbe621bf39733093c30c2
AnJiYeong <memarcis2@naver.com>
2022-12-30 11:38:25 +0900

    up

--- src/main/java/com/kh_semi/servlet_with_db/dao/JiyeongDB.java
+++ src/main/java/com/kh_semi/servlet_with_db/dao/JiyeongDB.java
@@ -6,7 +6,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class JiyeongDB {
+public class JiyeongDB { 
     /**
      * User테이블의 모든것을 가져오는 Function
      * 
@@ -38,6 +38,30 @@ public class JiyeongDB {
         }
         return user_list;
 
+    }
+
+    // SERVEY_INFO 테이블 전체를 가져오는 function
+    public ArrayList<HashMap> getServeysInfo(String getServeyInfo) throws SQLException{
+        
+        Commons commons = new Commons();
+        Statement statement = commons.getStatement();
+        String query = "SELECT * FROM SERVEY_INFO ";
+        ResultSet resultSet = statement.executeQuery(query);
+
+        ArrayList<HashMap> serveyInfo_list = new ArrayList<HashMap>();
+
+        while(resultSet.next()) {
+            HashMap<String, Object> serveyInfo = new HashMap<String, Object>();
+            serveyInfo.put("SERVEY_UID", resultSet.getString("SERVEY_UID"));
+            serveyInfo.put("SERVEY_NAME", resultSet.getString("SERVEY_NAME"));
+            serveyInfo.put("SERVEY_COMMENT", resultSet.getString("SERVEY_COMMENT"));
+            serveyInfo.put("SERVEY_START_DATE", resultSet.getString("SERVEY_START_DATE"));
+            serveyInfo.put("SERVEY_END_DATE", resultSet.getString("SERVEY_END_DATE"));
+            serveyInfo.put("SERVEY_UP_DATE", resultSet.getString("SERVEY_UP_DATE"));
+       
+            serveyInfo_list.add(serveyInfo);
+        }
+        return serveyInfo_list;
     }
 
 }
