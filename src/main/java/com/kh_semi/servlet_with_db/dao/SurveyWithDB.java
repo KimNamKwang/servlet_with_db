802359cb849454a9b2922a0680e1735461e64e92
dwogus6893 <dwogus6893@naver.com>
2022-12-31 18:11:02 +0900

    ss

--- src/main/java/com/kh_semi/servlet_with_db/dao/SurveyWithDB.java
+++ src/main/java/com/kh_semi/servlet_with_db/dao/SurveyWithDB.java
@@ -1,5 +1,79 @@
 package com.kh_semi.servlet_with_db.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 public class SurveyWithDB {
 
+    public HashMap<String, Object>getQuestion(String question_id) throws SQLException {
+
+        Commons commons = new Commons();
+        Statement statement = commons.getStatement();
+
+        String query = "SELECT * FROM QUESTION" +
+                " WHERE QUESTION_ID = '" + question_id + "'";
+
+        ResultSet resultSet = statement.executeQuery(query);
+        HashMap<String, Object> result = null;
+        while (resultSet.next()) {
+            result = new HashMap<>();
+                result.put("QUESTION", resultSet.getString("QUESTION"));
+                result.put("QUESTION_ID", resultSet.getString("QUESTION_ID"));
+                result.put("ORDERS", resultSet.getInt("ORDERS"));
+            }
+
+        return result;
+    }
+
+  
+    public ArrayList<HashMap> getAnswerList(String servey_id) throws SQLException{
+        
+        Commons commons = new Commons();
+        Statement statement = commons.getStatement();
+        String query = "SELECT ANSWER_LIST.QUESTION_ID, ANSWER.ORDERS, ANSWER.ANSWER" +
+                        "FROM ANSWER_LIST" +
+                        "INNER JOIN ANSWER" +
+                        "ON ANSWER_LIST.ANSWER_ID = ANSWER.ANSWER_ID " +
+                        "WHERE QUESTION_ID = '"+servey_id+"' "+
+                         "ORDER BY QUESTION_ID";
+        ResultSet resultSet = statement.executeQuery(query);
+
+        ArrayList<HashMap> answer_list = new ArrayList<HashMap>();
+        while(resultSet.next()){
+            HashMap<String, Object> answer = new HashMap<String, Object>();
+            answer.put("ANSWER_ID", resultSet.getString("ANSWER_ID"));
+            answer.put("ANSWER", resultSet.getString("ANSWER"));
+            answer.put("ORDERS", resultSet.getInt("ORDERS"));
+
+            answer_list.add(answer);
+        }
+        return answer_list;
+    }
+
+    // question 테이블 전체를 가져오는 function
+    public ArrayList<HashMap> getQuestionInfo(String questionInfo_id) throws SQLException{
+        
+        Commons commons = new Commons();
+        Statement statement = commons.getStatement();
+        String query = "SELECT * FROM QUESTION ";
+        ResultSet resultSet = statement.executeQuery(query);
+
+        ArrayList<HashMap> questionInfo_list = new ArrayList<HashMap>();
+ 
+        while(resultSet.next()) {
+            HashMap<String, Object> questionInfo = new HashMap<String, Object>();
+            questionInfo.put("QUESTION", resultSet.getString("QUESTION"));
+            questionInfo.put("QUESTION_ID", resultSet.getString("QUESTION_ID"));
+            questionInfo.put("ORDERS", resultSet.getString("ORDERS"));
+           
+       
+            questionInfo_list.add(questionInfo);
+        }
+        return questionInfo_list;
+    }
+        
 }
--- src/main/java/com/kh_semi/servlet_with_db/servlets/SurveyServlet.java
+++ src/main/java/com/kh_semi/servlet_with_db/servlets/SurveyServlet.java
@@ -1,14 +1,11 @@
 package com.kh_semi.servlet_with_db.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import org.springframework.cglib.proxy.Dispatcher;
-
-import com.kh_semi.servlet_with_db.dao.DatasWithDB;
+import com.kh_semi.servlet_with_db.dao.SurveyWithDB;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -16,41 +13,44 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+
 
 @WebServlet(urlPatterns = "/survey/Servlet")
 public class SurveyServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        
+        String question_id = request.getParameter("QUESTION_ID");
+
+
+        SurveyWithDB surveyWithDB = new SurveyWithDB();
+        HashMap<String, Object> question = null;
+        ArrayList<HashMap> answer_list = null;
+        
+        try {            
+            question  = surveyWithDB.getQuestion(question_id);
+            System.out.println(question.get("QUESTION"));
+            System.out.println(question.get("QUESTION_ID"));
+            System.out.println(question.get("ORDERS"));
+            answer_list = surveyWithDB.getAnswerList(question_id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        for(int i = 0; i < answer_list.size(); i++) {
+            HashMap<String, Object> answer =  answer_list.get(i);
+            System.out.println(answer.get("ANSWER"));
+            System.out.println(answer.get("ORDERS"));
+        }
+
+        request.setAttribute("servey_Info", question);
+        request.setAttribute("question_answer_list", answer_list);
+
         response.setContentType("text/html;charset=UTF-8"); // 응답을 보낼 때 한글이 깨지지 않게 해주는 것. 백엔드를 위한 것
-
-        DatasWithDB datasWithDB = new DatasWithDB();
-
-        ArrayList<HashMap<String, Object>> answersAndQuestionsList = null;
-        HashMap<String, Object> answersAndQuestions = null;
-        try {
-            answersAndQuestionsList = datasWithDB.getAnswersAndQuestions();
-        } catch (SQLException exception) {
-            exception.printStackTrace();
-        }
-
-        for (int i = 0; i < answersAndQuestionsList.size(); i++) {
-            answersAndQuestions = answersAndQuestionsList.get(i);
-            System.out.println(answersAndQuestions.get("QUESTION_ID"));
-            System.out.println(answersAndQuestions.get("QUESTION"));
-        }
-
-        request.setAttribute("answersAndQuestionsList", answersAndQuestionsList);
-
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/Survey.jsp");
+        
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/survey copy.jsp");
         requestDispatcher.forward(request, response);
 
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        /* super가 아니라 this를 쓰면 doGet이 실행되게 된다. */
-        this.doGet(req, resp);
     }
 
 }
