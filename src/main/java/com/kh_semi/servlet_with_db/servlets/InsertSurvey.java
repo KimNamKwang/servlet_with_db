d722b0562abe34e15632b83f3b6917c8ebbc5f45
dwogus6893 <dwogus6893@naver.com>
2023-01-03 00:52:23 +0900

    222

--- src/main/java/com/kh_semi/servlet_with_db/servlets/InsertSurvey.java
+++ src/main/java/com/kh_semi/servlet_with_db/servlets/InsertSurvey.java
@@ -6,14 +6,8 @@ import com.kh_semi.servlet_with_db.dao.Commons;
 
 
 public class InsertSurvey {
-    
-    public void insertSurvey(String user_id, String question_id, String answer_id) {
-        Commons commons = new Commons();
-        Statement statement = commons.getStatement();
 
-        //삽입칸
-    }
   
-    }
+
 
 }
